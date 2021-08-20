220811aaf129594b948a102e41da9d9e31275197
anupamsingh007 <75076385+anupamsingh007@users.noreply.github.com>
2021-08-20 13:21:03 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import "./styles.css";
+import React, { useState } from "react";
+var emojiDictionary = {
+  "🍕": "Pizza",
+  "🍔": "Burger",
+  "🍟": "French Fries",
+  "🌭": "HotDog",
+  "🎂": "Cake",
+  "🍰": "Pastry",
+  "🍺": "Beer",
+  "🍸": "Cocktail",
+  "🍌": "Banana",
+  "🥕": "Carrot",
+  "🍗": "Chicken"
+};
+
+var emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function inputEmojiHandler(event) {
+    var userInput = event.target.value;
+    var matchedMeaning = emojiDictionary[userInput];
+    setMeaning(matchedMeaning);
+  }
+
+  function clickEmojiHandler(emoji) {
+    var matchedEmoji = emojiDictionary[emoji];
+    setMeaning(matchedEmoji);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Interpreter</h1>
+      <p>
+        Search for food items with emoji or click on emoji to see the name of
+        the food.
+      </p>
+      <input onChange={inputEmojiHandler} />
+      <h2>{meaning}</h2>
+      <h3>emojis we know</h3>
+      <div className="emojis">
+        {emojisWeKnow.map((emoji) => {
+          return (
+            <span onClick={() => clickEmojiHandler(emoji)} key={emoji}>
+              {emoji}
+            </span>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
